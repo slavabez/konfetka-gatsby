0e1c8ebeee700be37d22f26aa517f7642fd2d09f
Slava Bezgachev <bezgachev@gmail.com>
2020-05-12 15:35:26 +0600

    testing chakra

--- src/gatsby-plugin-chakra-ui/theme.ts
+++ src/gatsby-plugin-chakra-ui/theme.ts
@@ -1,1 +1,24 @@
-export { theme as default } from '@chakra-ui/core';
+import { theme } from '@chakra-ui/core';
+
+// Override system the default Chakra theme
+
+theme.breakpoints = ['30em', '48em', '62em', '80em'];
+theme.fonts = {
+  heading: '"Avenir Next", sans-serif',
+  body: 'system-ui, sans-serif',
+  mono: 'Menlo, monospace',
+};
+theme.fontSizes = {
+  xs: '0.75rem',
+  sm: '0.875rem',
+  md: '1rem',
+  lg: '1.125rem',
+  xl: '1.25rem',
+  '2xl': '1.5rem',
+  '3xl': '1.875rem',
+  '4xl': '2.25rem',
+  '5xl': '3rem',
+  '6xl': '4rem',
+};
+
+export default theme;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,7 @@
+import {
+  Stack,
+  Text,
+} from '@chakra-ui/core';
 import React from 'react';
 
 import { Layout } from '../components/Layout';
@@ -5,7 +9,19 @@ import { Layout } from '../components/Layout';
 export default function IndexPage(): JSX.Element {
   return (
     <Layout>
-      <p>Hello, world!</p>
+
+      <Stack spacing={2}>
+        <Text fontSize="6xl">Такой вот текст</Text>
+        <Text fontSize="5xl">Такой вот текст</Text>
+        <Text fontSize="4xl">Такой вот текст</Text>
+        <Text fontSize="3xl">Такой вот текст</Text>
+        <Text fontSize="2xl">Такой вот текст</Text>
+        <Text fontSize="xl">Такой вот текст</Text>
+        <Text fontSize="lg">Такой вот текст</Text>
+        <Text fontSize="md">Такой вот текст</Text>
+        <Text fontSize="sm">Такой вот текст</Text>
+        <Text fontSize="xs">Такой вот текст</Text>
+      </Stack>
     </Layout>
   );
 }
