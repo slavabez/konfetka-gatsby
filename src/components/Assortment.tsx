a542b5210cba7f2eac1dc38fa863d6526bedc6fb
Slava Bezgachev <bezgachev@gmail.com>
2020-05-21 16:38:07 +0600

    Re-generated GQ

--- src/components/Assortment.tsx
+++ src/components/Assortment.tsx
@@ -1,7 +1,26 @@
 import { Box, Heading } from '@chakra-ui/core';
+import { graphql, useStaticQuery } from 'gatsby';
 import React from 'react';
 
 const AssortmentSection = () => {
+  const data = useStaticQuery(graphql`
+    {
+      allAssetsJson {
+        edges {
+          node {
+            products {
+              image
+              name
+              description
+              price
+            }
+          }
+        }
+      }
+    }
+
+  `);
+
   return (
     <Box as="section" m="2">
       <Heading as="h1" size="xl" textAlign="center">
